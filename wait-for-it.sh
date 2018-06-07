72d62578926f104af9e143b445aa1af9dc1693fc
Simon Morley <simon@polkaspots.com>
2018-06-07 18:36:25 +0100

    updates waits...

---
+++ wait-for-it.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+hostname="$1"
+shift
+cmd="$@"
+
+echo $hostname
+cursor=.
+while true ; do
+  response=$(curl --write-out %{http_code} -k --silent --output /dev/null $hostname)
+  if [ "${response}" == 209 ] ; then
+    break
+  fi
+  cursor=$cursor.
+  echo -ne "$hostname is not up, sleeping$cursor\r"
+  sleep 3
+done
+
+>&2 echo "API is up - executing command"
+exec $cmd
