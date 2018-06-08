3a50b16711ec4e6cf1309b7768d6d4dc5ec123f7
Simon Morley <simon@polkaspots.com>
2018-06-08 17:14:38 +0100

    updates

--- docker-install.sh
+++ docker-install.sh
@@ -426,7 +426,7 @@ update_config() {
   echo 'API started, creating the SSL certificates'
   echo
 
-  docker-compose -f docker-compose-lets-encrypt.yml up >> /dev/null 2>&1 &
+  docker-compose -f docker-compose-lets-encrypt.yml up -d
 
   until $(docker ps | grep -q letsencrypt); do
     cursor=$cursor.
