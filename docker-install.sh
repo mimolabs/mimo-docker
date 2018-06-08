82bcab32bc1af31bd33272c302b860b5aeff9fd0
Simon Morley <simon@polkaspots.com>
2018-06-08 11:43:31 +0100

    updates

--- docker-install.sh
+++ docker-install.sh
@@ -404,7 +404,7 @@ update_config() {
   #   docker-compose pull && docker-compose up --force-recreate
     docker-compose up
   else
-    docker-compose up -d
+    docker-compose down; docker-compose -f docker-compose.yml -f docker-compose-lets-encrypt.yml up -d
     # --force-recreate
   fi
 
